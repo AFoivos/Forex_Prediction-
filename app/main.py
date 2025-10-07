12e0b75cf62581ea6c13598ff36b6702f5c7db55
FGADev <ampatzisfoivos@gmail.com>
2025-10-07 16:14:49 +0000

    ch

--- app/main.py
+++ app/main.py
@@ -10,11 +10,31 @@ from app.routers import (
 
 app = FastAPI(title="Forex Analysis API")
 
-app.include_router(data_handle_router.router, prefix="/api/v1/data", tags=["Data"])
-app.include_router(features_router.router, prefix="/api/v1/features", tags=["Features"])
-app.include_router(analysis_router.router, prefix="/api/v1/signals", tags=["Signals"])
-app.include_router(models_router.router, prefix="/api/v1/analysis", tags=["Analysis"])
-app.include_router(plots_router.router, prefix="/api/v1/predict", tags=["Predictions"])
+app.include_router(
+    data_handle_router.router, 
+    prefix = "/api/v1/data", 
+    tags = ["Data"]
+)
+app.include_router(
+    features_router.router, 
+    prefix = "/api/v1/features", 
+    tags = ["Features"]
+)
+app.include_router(
+    analysis_router.router, 
+    prefix = "/api/v1/signals", 
+    tags = ["Signals"]
+)
+app.include_router(
+    models_router.router, 
+    prefix = "/api/v1/analysis", 
+    tags = ["Analysis"]
+)
+app.include_router(
+    plots_router.router, 
+    prefix = "/api/v1/predict", 
+    tags = ["Predictions"]
+)
 
 @app.get("/")
 def root():
